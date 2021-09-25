e1639be33971ee1a9a2ceb49f2f96c4a6dd86623
Justin Aquadro <jaquadro@gmail.com>
2021-09-25 18:36:02 -0400

    ACL plugin to use video player lock as bypass

---
+++ Assets/Texel/ACL/VideoLockAclHandler.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Texel
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class VideoLockAclHandler : UdonSharpBehaviour
+    {
+        public AccessControl acl;
+        public SyncPlayer videoPlayer;
+
+        [NonSerialized]
+        public VRCPlayerApi playerArg;
+        [NonSerialized]
+        public int checkResult;
+
+        const int RESULT_ALLOW = 1;
+        const int RESULT_PASS = 0;
+        const int RESULT_DENY = -1;
+
+        void Start()
+        {
+            acl._RegsiterAccessHandler(this, "_CheckAccess", "playerArg", "checkResult");
+        }
+
+        public void _CheckAccess()
+        {
+            if (!videoPlayer.locked)
+                checkResult = RESULT_ALLOW;
+            else
+                checkResult = RESULT_PASS;
+        }
+    }
+}
