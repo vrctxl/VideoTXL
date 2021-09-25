f25d0d34233225024dfcb72989ca840716bb14d2
Justin Aquadro <jaquadro@gmail.com>
2021-09-25 18:55:57 -0400

    More local controls and misc

---
+++ Assets/Texel/Video/UI/Scripts/LocalControlsSlim.cs
@@ -0,0 +1,90 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Texel
+{
+    [AddComponentMenu("VideoTXL/UI/Local Controls Slim")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class LocalControlsSlim : UdonSharpBehaviour
+    {
+        public UdonBehaviour videoPlayer;
+        public AudioManager audioManager;
+
+        [Header("Internal")]
+        public GameObject muteToggleOn;
+        public GameObject muteToggleOff;
+        public Slider volumeSlider;
+
+        Color normalColor = new Color(1f, 1f, 1f, .8f);
+        Color disabledColor = new Color(.5f, .5f, .5f, .4f);
+        Color activeColor = new Color(0f, 1f, .5f, .7f);
+        Color attentionColor = new Color(.9f, 0f, 0f, .5f);
+
+        void Start()
+        {
+            if (Utilities.IsValid(audioManager))
+                audioManager._RegisterControls(this);
+        }
+
+        bool inVolumeControllerUpdate = false;
+
+        public void _AudioManagerUpdate()
+        {
+            if (!Utilities.IsValid(audioManager))
+                return;
+
+            inVolumeControllerUpdate = true;
+
+            if (Utilities.IsValid(volumeSlider))
+            {
+                float volume = audioManager.masterVolume;
+                if (volume != volumeSlider.value)
+                    volumeSlider.value = volume;
+            }
+
+            UpdateToggleVisual();
+
+            inVolumeControllerUpdate = false;
+        }
+
+        public void _Resync()
+        {
+            if (Utilities.IsValid(videoPlayer))
+                videoPlayer.SendCustomEvent("_Resync");
+        }
+
+        public void _ToggleVolumeMute()
+        {
+            if (inVolumeControllerUpdate)
+                return;
+
+            if (Utilities.IsValid(audioManager))
+                audioManager._SetMasterMute(!audioManager.masterMute);
+        }
+
+        public void _UpdateVolumeSlider()
+        {
+            if (inVolumeControllerUpdate)
+                return;
+
+            if (Utilities.IsValid(audioManager) && Utilities.IsValid(volumeSlider))
+                audioManager._SetMasterVolume(volumeSlider.value);
+        }
+
+        void UpdateToggleVisual()
+        {
+            if (Utilities.IsValid(audioManager))
+            {
+                if (Utilities.IsValid(muteToggleOn) && Utilities.IsValid(muteToggleOff))
+                {
+                    muteToggleOn.SetActive(audioManager.masterMute);
+                    muteToggleOff.SetActive(!audioManager.masterMute);
+                }
+            }
+        }
+    }
+}
