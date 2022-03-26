891655ec01d147ed696e1161c20436bbb7ad2acc
Justin Aquadro <jaquadro@gmail.com>
2022-03-25 21:31:21 -0400

    URL Control to load URL via interact

---
+++ Assets/Texel/Video/UI/Scripts/UrlControl.cs
@@ -0,0 +1,37 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Texel
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class UrlControl : UdonSharpBehaviour
+    {
+        public SyncPlayer syncPlayer;
+        public LocalPlayer localPlayer;
+
+        public VRCUrl url;
+
+        public void _Trigger()
+        {
+            if (Utilities.IsValid(syncPlayer))
+            {
+                syncPlayer._ChangeUrl(url);
+            }
+
+            if (Utilities.IsValid(localPlayer))
+            {
+                localPlayer.streamUrl = url;
+                localPlayer._TriggerStop();
+                localPlayer._TriggerPlay();
+            }
+        }
+
+        public override void Interact()
+        {
+            _Trigger();
+        }
+    }
+}
