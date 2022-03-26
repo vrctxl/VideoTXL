1e2ff1039bb07eadace496c35f6894a003a4a5cf
Justin Aquadro <jaquadro@gmail.com>
2022-03-25 21:27:38 -0400

    zone membership registered callback support

--- Assets/Texel/General/ZoneMembership.cs
+++ Assets/Texel/General/ZoneMembership.cs
@@ -3,6 +3,7 @@ using System;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
+using VRC.Udon;
 
 namespace Texel
 {
@@ -16,9 +17,26 @@ namespace Texel
         int[] players = new int[100];
         int maxIndex = -1;
 
+        int handlerCount = 0;
+        Component[] targetBehaviors;
+        string[] eventNames;
+        string[] arrayReturns;
+
         void Start()
         {
 
+        }
+
+        public void _RegisterArrayUpdateHandler(UdonBehaviour target, string eventName, string arrayReturn)
+        {
+            if (!Utilities.IsValid(target) || eventName == "" || arrayReturn == "")
+                return;
+
+            targetBehaviors = (UdonBehaviour[])_AddElement(targetBehaviors, target, typeof(UdonBehaviour));
+            eventNames = (string[])_AddElement(eventNames, eventName, typeof(string));
+            arrayReturns = (string[])_AddElement(arrayReturns, arrayReturn, typeof(string));
+
+            handlerCount += 1;
         }
 
         public void _PlayerTriggerEnter()
@@ -31,26 +49,28 @@ namespace Texel
             _RemovePlayer(playerEventArg);
         }
 
-        public void _AddPlayer(VRCPlayerApi player)
+        public int _AddPlayer(VRCPlayerApi player)
         {
             if (!Utilities.IsValid(player))
-                return;
+                return -1;
 
             if (!player.IsValid())
             {
                 _RemovePlayer(player);
-                return;
+                return -1;
             }
 
             int id = player.playerId;
             for (int i = 0; i <= maxIndex; i++)
             {
                 if (players[i] == id)
-                    return;
+                    return i;
             }
 
             maxIndex += 1;
             players[maxIndex] = id;
+
+            return maxIndex;
         }
 
         public void _RemovePlayer(VRCPlayerApi player)
@@ -65,6 +85,17 @@ namespace Texel
                 {
                     players[i] = players[maxIndex];
                     maxIndex -= 1;
+
+                    for (int j = 0; j < handlerCount; j++)
+                    {
+                        UdonBehaviour target = (UdonBehaviour)targetBehaviors[j];
+                        target.SendCustomEvent(eventNames[j]);
+                        Array arr = (Array)target.GetProgramVariable(arrayReturns[j]);
+
+                        if (Utilities.IsValid(arr) && arr.Length > maxIndex)
+                            arr.SetValue(arr.GetValue(maxIndex), i);
+                    }
+
                     return;
                 }
             }
@@ -87,19 +118,42 @@ namespace Texel
             return VRCPlayerApi.GetPlayerById(id);
         }
 
-        public bool _ContainsPlayer(VRCPlayerApi player)
+        public int _GetPlayerIndex(VRCPlayerApi player)
         {
             if (!Utilities.IsValid(player))
-                return false;
+                return -1;
 
             int id = player.playerId;
             for (int i = 0; i <= maxIndex; i++)
             {
                 if (players[i] == id)
-                    return true;
+                    return i;
             }
 
-            return false;
+            return -1;
+        }
+
+        public bool _ContainsPlayer(VRCPlayerApi player)
+        {
+            return _GetPlayerIndex(player) > -1;
+        }
+
+        Array _AddElement(Array arr, object elem, Type type)
+        {
+            Array newArr;
+            int count = 0;
+
+            if (Utilities.IsValid(arr))
+            {
+                count = arr.Length;
+                newArr = Array.CreateInstance(type, count + 1);
+                Array.Copy(arr, newArr, count);
+            }
+            else
+                newArr = Array.CreateInstance(type, 1);
+
+            newArr.SetValue(elem, count);
+            return newArr;
         }
     }
 }
