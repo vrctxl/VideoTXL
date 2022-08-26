ded95b15b8d601895cbc1f7fe4149f832e66b630
Justin Aquadro <jaquadro@gmail.com>
2022-08-26 08:06:04 -0400

    Add forwarding ACL Handler

---
+++ Assets/Texel/ACL/ForwardingAclHandler.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Texel
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class ForwardingAclHandler : UdonSharpBehaviour
+    {
+        public AccessControl acl;
+        public AccessControl[] forwardAcls;
+
+        [NonSerialized]
+        public VRCPlayerApi playerArg;
+        [NonSerialized]
+        public int checkResult;
+
+        const int RESULT_ALLOW = 1;
+        const int RESULT_PASS = 0;
+        const int RESULT_DENY = -1;
+
+        void Start()
+        {
+            acl._RegsiterAccessHandler(this, "_CheckAccess", "playerArg", "checkResult");
+        }
+
+        public void _CheckAccess()
+        {
+            for (int i = 0; i < forwardAcls.Length; i++)
+            {
+                if (!Utilities.IsValid(forwardAcls[i]))
+                    continue;
+
+                if (forwardAcls[i]._HasAccess(playerArg))
+                {
+                    checkResult = RESULT_ALLOW;
+                    return;
+                }
+            }
+
+            checkResult = RESULT_PASS;
+        }
+    }
+}
