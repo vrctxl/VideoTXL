1d0a1088d727fcfe5575f5ff050634a95b643a37
Justin Aquadro <jaquadro@gmail.com>
2023-07-30 12:14:30 -0400

    context menus

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
 - Removed reference to Video Manager and Audio Manager from Sync Video Player
 - Video Manager and Audio Manager objects register themselves with a video player on startup
 - Updated sync video player integrity check to look for none or duplicate managers referencing it
+- Added menu items to GameObject menu to add different VideoTXL objects to scene
 
 ## [2.0.0] - 07-29-23
 
---
+++ Packages/com.texelsaur.video/Editor/VideoTxlManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Texel
+{
+    [InitializeOnLoad]
+    public class VideoTxlManager
+    {
+        [MenuItem("Tools/TXL/VideoTXL/Add \"Sync Video Player\" Prefab to Scene", false)]
+        [MenuItem("GameObject/TXL/VideoTXL", false, 49)]
+        [MenuItem("GameObject/TXL/VideoTXL/Sync Video Player", false, 100)]
+        public static void AddSyncPlayerToScene()
+        {
+            AddPrefabToScene("Packages/com.texelsaur.video/Runtime/Prefabs/Sync Video Player.prefab");
+        }
+
+        [MenuItem("GameObject/TXL/VideoTXL/Other Video Players", false, 200)]
+        [MenuItem("GameObject/TXL/VideoTXL/Other Video Players/Basic Sync Video Player", false, 201)]
+        public static void AddBasicSyncPlayerToScene()
+        {
+            AddPrefabToScene("Packages/com.texelsaur.video/Runtime/Prefabs/Other Video Players/Basic Sync Video Player.prefab");
+        }
+
+        [MenuItem("GameObject/TXL/VideoTXL/Other Video Players/Local Video Player AVPro", false, 202)]
+        public static void AddLocalAVProPlayerToScene()
+        {
+            AddPrefabToScene("Packages/com.texelsaur.video/Runtime/Prefabs/Other Video Players/Local Video Player AVPro.prefab");
+        }
+
+        [MenuItem("GameObject/TXL/VideoTXL/Other Video Players/Local Video Player Unity", false, 203)]
+        public static void AddLocalUnityPlayerToScene()
+        {
+            AddPrefabToScene("Packages/com.texelsaur.video/Runtime/Prefabs/Other Video Players/Local Video Player Unity.prefab");
+        }
+
+        [MenuItem("GameObject/TXL/VideoTXL/Other Video Players/Sync Video Player Full", false, 204)]
+        public static void AddSyncPlayerFullToScene()
+        {
+            AddPrefabToScene("Packages/com.texelsaur.video/Runtime/Prefabs/Other Video Players/Sync Video Player Full.prefab");
+        }
+
+        [MenuItem("GameObject/TXL/VideoTXL/UI", false, 210)]
+        [MenuItem("GameObject/TXL/VideoTXL/UI/Sync Video Player Controls", false, 211)]
+        public static void AddSyncPlayerControlsToScene()
+        {
+            GameObject asset = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/com.texelsaur.video/Runtime/UI/Sync Player/PlayerControls.prefab");
+            if (asset != null)
+            {
+                GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(asset);
+
+                if (Selection.activeTransform) {
+                    SyncPlayer vp = Selection.activeTransform.GetComponent<SyncPlayer>();
+                    if (!vp)
+                        vp = Selection.activeTransform.GetComponentInParent<SyncPlayer>();
+
+                    if (vp)
+                    {
+                        PlayerControls com = instance.GetComponent<PlayerControls>();
+                        if (com)
+                            com.videoPlayer = vp;
+
+                        AudioManager audioMan = vp.GetComponentInChildren<AudioManager>();
+                        if (audioMan && audioMan.videoPlayer == vp)
+                            com.audioManager = audioMan;
+
+                        instance.transform.SetParent(vp.transform);
+                    }
+                }
+
+                EditorGUIUtility.PingObject(instance);
+            }
+        }
+
+        [MenuItem("GameObject/TXL/VideoTXL/UI/Local Video Player Controls", false, 212)]
+        public static void AddLocalPlayerControlsToScene()
+        {
+            GameObject asset = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/com.texelsaur.video/Runtime/UI/Local Controls/LocalControlsSlim.prefab");
+            if (asset != null)
+            {
+                GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(asset);
+
+                if (Selection.activeTransform)
+                {
+                    TXLVideoPlayer vp = Selection.activeTransform.GetComponent<TXLVideoPlayer>();
+                    if (!vp)
+                        vp = Selection.activeTransform.GetComponentInParent<TXLVideoPlayer>();
+
+                    if (vp)
+                    {
+                        LocalControlsSlim com = instance.GetComponent<LocalControlsSlim>();
+                        if (com)
+                            com.videoPlayer = vp;
+
+                        AudioManager audioMan = vp.GetComponentInChildren<AudioManager>();
+                        if (audioMan && audioMan.videoPlayer == vp)
+                            com.audioManager = audioMan;
+
+                        instance.transform.SetParent(vp.transform);
+                    }
+                }
+
+                EditorGUIUtility.PingObject(instance);
+            }
+        }
+
+        public static void AddPrefabToScene(string path)
+        {
+            GameObject asset = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            if (asset != null)
+            {
+                GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(asset);
+                EditorGUIUtility.PingObject(instance);
+            }
+        }
+    }
+}
